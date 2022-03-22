b6e64e33df22f76cb232110905e2dd489a20a771
Clark Brace <cbrace@pugetsound.edu>
2022-03-21 21:26:44 -0700

    got the par working with even values! need to refine for more nuanced inputs

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 /* globals (anything here would be shared with all threads) */
 double **matrixOne;
 double **matrixTwo;
-double **matrixProduct;
+double **P_matrixProduct;
+double **S_matrixProduct;
 int matrixSize;
 int threadCount;
 int seqORPar; //0 == seq, 1 = paraless
@@ -16,6 +17,8 @@ int seqORPar; //0 == seq, 1 = paraless
 //argc is the number of arguments 0 being ./mmm
 int main(int argc, char *argv[])
 {
+	//Make sure there the minimum number of arguments
+	//Print out for now
 	int i;
 	for (int i = 0; i < argc; ++i){
 		printf("%d :%s\n" ,i,argv[i]);
@@ -23,27 +26,64 @@ int main(int argc, char *argv[])
         
 
 //May need to add a check about if you ender the wronge S or P command
-	if(!strcmp(argv[1], "S") || !strcmp(argv[1], "s")){ 
+	if(!strcmp(argv[1], "S") || !strcmp(argv[1], "s")){
 		printf("\nS!\n"); //REMOVE
+		
+		//Checks to make sure input is correct for Sequental	
+		if(argc < 3){
+			printf("Error: too few arguments\n");
+			return 1;
+		}else if(argc > 3){
+			printf("Error: too many arguments\n");
+			return 1;
+		}
 			seqORPar = 0;//set to sequental path
 			matrixSize = atoi(argv[2]); //set matrix size to the correct size
 
 	}else if(!strcmp(argv[1], "P") || !strcmp(argv[1], "p")){
 		printf("\nP!\n"); //REMOVE
+		
+		//Checks to make sure input is correct
+		if(argc < 4 ){
+			printf("Error: parallel mode requires <size>\n");
+			return 1;
+		}else if(argc > 4){
+			printf("Error: too many arguments\n");
+		return 1;
+		}
 		//Set inital perameters:
-			seqORPar = 1; //set to paralell path
-			threadCount = atoi(argv[2]); //set thread count to the correct number
-			matrixSize = atoi(argv[3]); //set matrix size to the correct size
+		seqORPar = 1; //set to paralell path
+		threadCount = atoi(argv[2]); //set thread count to the correct number
+		matrixSize = atoi(argv[3]); //set matrix size to the correct size
+
+		//TODO need to make both the paralell and sequental run and compare times and then compare
 		
 	}else{
-		printf("\nSorry, your input of %c is not a valid input. Please only use inputs of (S or P)\nUsage: ./mmm <mode> [num threads] <size>\n", *argv[1]); //ASK print out as string reguardless?
+		printf("\nError: your input of %c is not a valid input. Please only use inputs of (S or P)\nUsage: ./mmm <mode> [num threads] <size>\n", *argv[1]); //ASK print out as string reguardless?
 		return 0;
 	}
 	
-	mmm_init();
-	printMatrix(matrixOne);
-	mmm_reset(matrixOne);
-	printMatrix(matrixOne);
+	mmm_init(); //always want to start things up!
+
+	mmm_seq();
+	printf("\nMatrix Product Seq\n");
+	printMatrix(S_matrixProduct);
+	parallel();
+	printf("\nMatrix Product Par\n");
+	printMatrix(P_matrixProduct);
+	
+	// printf("\nMatrix 1\n");
+	// printMatrix(matrixOne);
+	// printf("\nMatrix 2\n");
+	// printMatrix(matrixTwo);
+
+	// printf("\nMatrix Product\n");
+	// printMatrix(S_matrixProduct);
+	// mmm_seq();
+	// printf("\nMatrix Product After\n");
+	// printMatrix(S_matrixProduct);
+	// //reset test
+	// mmm_reset(S_matrixProduct);
 
 	//matrixSize = argv[1];
 
@@ -62,3 +102,5 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+
+
--- mmm.h
+++ mmm.h
@@ -3,10 +3,20 @@
 
 extern double **matrixOne;
 extern double **matrixTwo;
-extern double **matrixProduct;
+extern double **S_matrixProduct; //for the sequental result
+extern double **P_matrixProduct; //for the paralell result
 extern int matrixSize;
+extern int threadCount;
+
+typedef struct thread_args
+ {
+	 int tid;
+	 int firstRow;
+	 int LastRow;
+ }thread_args;
 
 void mmm_init();
+
 
 void malMatrix(double **matrix); //to malloc the matricies 
 void rNumToMatrix(double **matrix); //to populate the matricies
@@ -14,7 +24,10 @@ void rNumToMatrix(double **matrix); //to populate the matricies
 void mmm_reset(double **matrix);
 void mmm_freeup();
 void mmm_seq();
-void *mmm_par(void *args);
+void *mmm_par();
+
+void parallel();
+
 double mmm_verify();
 
 void printMatrix(double **matrix); //to test and print matricies
